7f08fff04e53f64810a07f400d223aadade5fa85
Tomo Suzuki <tomo@soultrust.com>
2022-12-29 22:16:08 -0500

    Fix rotation value not working after 2 clicks

--- src/components/PanePanel.js
+++ src/components/PanePanel.js
@@ -1,43 +1,23 @@
+import { useState } from 'react';
+
 const PanePanel = ({ side, img }) => {
+  const [rotationVal, setRotationVal] = useState(0);
 
   const handleClick = (evt) => {
     if (evt.currentTarget.classList.contains('pane-panel')) {
-      
       const bgHolder = evt.currentTarget.querySelector('.bg-holder');
-      var st = window.getComputedStyle(bgHolder, null);
-      var tr = st.getPropertyValue("-webkit-transform") ||
-        st.getPropertyValue("-moz-transform") ||
-        st.getPropertyValue("-ms-transform") ||
-        st.getPropertyValue("-o-transform") ||
-        st.getPropertyValue("transform") ||
-        "fail...";
-
-      console.log('Matrix: ' + tr);
-
-      var values = tr.split('(')[1];
-      values = values.split(')')[0];
-      values = values.split(',');
-      var a = values[0];
-      var b = values[1];
-
-      var scale = Math.sqrt(a*a + b*b);
-
-      // arc sin, convert from radians to degrees, round
-      // DO NOT USE: see update below
-      var sin = b/scale;
-      var angle = Math.round(Math.asin(sin) * (180/Math.PI));
-
-      // works!
-      console.log('Rotate: ' + angle + 'deg');
-
-      const newRotation = angle + 90;
-      bgHolder.style.transform = `rotateZ(${newRotation}deg)`;
+      setRotationVal(rotationVal + 90);
     }
   }
 
   return (
     <div className={`pane-panel pane-${side}`} onClick={handleClick}>
-      <div className="bg-holder" style={{ backgroundImage: `url(${img})`}}></div>
+      <div 
+        className="bg-holder" 
+        style={{ 
+          backgroundImage: `url(${img})`, 
+          transform: `rotateZ(${rotationVal}deg)`
+        }}></div>
     </div>
   );
 };
